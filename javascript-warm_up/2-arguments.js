64184348535efc58a9553c9b4e0594fa0ec17802
harshav2101 <you@example.com>
2023-03-06 05:16:21 +0000

    task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
-f (process.argv.length < 3) {
+#!/usr/bin/node
+if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
